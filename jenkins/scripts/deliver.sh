e16c237ce6905cc531a10358c15202e586638c22
wuyong-hub <wuyong@bsoft.com.cn>
2021-01-06 12:08:32 +0800

    dsfdsafdsafsad

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -4,20 +4,20 @@ echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
-set -x
-/usr/local/apache-maven-3.6.3/bin/mvn jar:jar install:install help:evaluate -Dexpression=project.name
-set +x
+#set -x
+#mvn jar:jar install:install help:evaluate -Dexpression=project.name
+#set +x
 
 echo 'The following complex command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME=`/usr/local/apache-maven-3.6.3/bin/mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
+NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
 set +x
 
 echo 'The following complex command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-VERSION=`/usr/local/apache-maven-3.6.3/bin/mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
+VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
