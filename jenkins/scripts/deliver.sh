25111fab580f343bed0a09d4c24580696647d300
wuyong-hub <wuyong@bsoft.com.cn>
2021-01-08 15:30:45 +0800

    dfadfdsadsag

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -11,13 +11,13 @@ echo 'volume).'
 echo 'The following complex command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
+NAME=`/usr/local/apache-maven-3.6.3/mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
 set +x
 
 echo 'The following complex command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
+VERSION=`/usr/local/apache-maven-3.6.3/mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
@@ -26,6 +26,7 @@ echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 #java -jar target/${NAME}-${VERSION}.jar
 
 echo 'remove old image.'
+set -x
 CONTAINERID=`docker ps -a | grep blue | awk '{print $1}'`
 if [$CONTAINERID] 
 then
@@ -33,14 +34,19 @@ then
    docker container rm $CONTAINERID
    docker image rm ${NAME}
 fi
+set +x
 
 echo 'build docker image.'
 mkdir docker-build
 cp target/${NAME}-${VERSION}.jar docker-build/app.jar
 cp Dockerfile docker-build/
 cd dcoker-build
+set -x
 docker build -t ${NAME}:${VERSION}
+set +x
 
 echo 'run image.'
+set -x
 docker run -d -p 9900:9900 ${NAME}:${VERSION}
+set +x
 
